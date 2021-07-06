e10d885b4706e915e7290494373e4a3f21f3e1f4
Sergey Imangulov <65292943+SergeyImangulov@users.noreply.github.com>
2021-07-06 11:04:48 +0400

    Update index.php
    Added the "require_once" command to connect the "WriteToLog.php" file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Форма заявления</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-
+    <? require_once "WriteToLog.php"?>
 </head>
 <body>
   <h1>Форма заявления</h1>
@@ -21,65 +21,3 @@
     </form>
 </body>
 </html>
-<?
-/**
- * Write data to log file.
- *
- * @param mixed $data
- * @param string $title
- *
- * @return bool
- */
-function writeToLog($data, $title = '') {
- $log = "\n------------------------\n";
- $log .= date("Y.m.d G:i:s") . "\n";
- $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
- $log .= print_r($data, 1);
- $log .= "\n------------------------\n";
- file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
- return true;
-}
-
-$defaults = array('first_name' => '', 'last_name' => '', 'phone' => '', 'email' => '');
-
-if (array_key_exists('saved', $_REQUEST)) {
- $defaults = $_REQUEST;
- writeToLog($_REQUEST, 'webform');
-
- $queryUrl = '...'; // Here is the url of your incoming webhook using the generated crm.lead.add method
- $queryData = http_build_query(array(
- 'fields' => array(
- "TITLE" => 'Заявка от'.' '.$_REQUEST['first_name'].' '.$_REQUEST['last_name'],
- "NAME" => $_REQUEST['first_name'],
- "LAST_NAME" => $_REQUEST['last_name'],
- "COMMENTS" => $_REQUEST['comments'],
- "SOURCE_ID" => "CRM",
- "STATUS_ID" => "NEW",
- "OPENED" => "Y",
- "ASSIGNED_BY_ID" => 1,
- "PHONE" => array(array("VALUE" => $_REQUEST['phone'], "VALUE_TYPE" => "WORK" )),
- "EMAIL" => array(array("VALUE" => $_REQUEST['email'], "VALUE_TYPE" => "WORK" )),
- ),
- 'params' => array("REGISTER_SONET_EVENT" => "Y")
- ));
-
- $curl = curl_init();
- curl_setopt_array($curl, array(
- CURLOPT_SSL_VERIFYPEER => 0,
- CURLOPT_POST => 1,
- CURLOPT_HEADER => 0,
- CURLOPT_RETURNTRANSFER => 1,
- CURLOPT_URL => $queryUrl,
- CURLOPT_POSTFIELDS => $queryData,
- ));
-
- $result = curl_exec($curl);
- curl_close($curl);
-
- $result = json_decode($result, 1);
- writeToLog($result, 'webform result');
-
- if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description']."<br/>";
-}
-
-?>
